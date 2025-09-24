766a4e5c2102815d2d11d0f9f02bfdf5993b2635
Edwin Bosire <me@edwinb.co.uk>
2025-09-24 01:39:02 +0100

    renamed source folder

---
+++ Source/ContextMenu/ContextMenuApp.swift
@@ -0,0 +1,17 @@
+//
+//  ContextMenuApp.swift
+//  ContextMenu
+//
+//  Created by Edwin Bosire on 23/09/2025.
+//
+
+import SwiftUI
+
+@main
+struct ContextMenuApp: App {
+    var body: some Scene {
+        WindowGroup {
+			ContextMenu()
+        }
+    }
+}
---
+++ Source/ContextMenuTests/ContextMenuTests.swift
@@ -0,0 +1,16 @@
+//
+//  ContextMenuTests.swift
+//  ContextMenuTests
+//
+//  Created by Edwin Bosire on 23/09/2025.
+//
+
+import Testing
+
+struct ContextMenuTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
